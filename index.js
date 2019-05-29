b05b54fa897884279bf9cccc7c9d5e23a72be5a5
Alex Mason <alexmason@DF-660720.local>
2019-05-29 11:50:19 -0400

    Added eslint

--- index.js
+++ index.js
@@ -1,71 +1,70 @@
 
 
-const ToDo = require('./todo');
 const express = require('express');
 const expressValidator = require('express-validator');
+const ToDo = require('./todo');
+
 const app = express();
 app.use(express.json());
 app.use(expressValidator());
 
 const aList = new ToDo('AlexsList');
 
-aList.add_item({
-    id:"1",
-    name:"item1",
-    description:"Description . . ",
-    tags:['high priority'],
-    status:true
+aList.addItem({
+  id: '1',
+  name: 'item1',
+  description: 'Description . . ',
+  tags: ['high priority'],
+  status: true,
 });
-aList.add_item({
-    id:"2",
-    name:"item2",
-    description:"This is my second list item",
-    tags:['low priority', 'chores'],
-    status:true
+aList.addItem({
+  id: '2',
+  name: 'item2',
+  description: 'This is my second list item',
+  tags: ['low priority', 'chores'],
+  status: true,
 });
 
-//Landing page, welcome message
+// Landing page, welcome message
 app.get('/', (req, res) => {
-    res.send('welcome to my TODO api');
+  res.send('welcome to my TODO api');
 });
 
-//our full list of ToDos
+// our full list of ToDos
 app.get('/api/list', (req, res) => {
-    res.send(aList.get_list());
+  res.send(aList.getList());
 });
 
 app.post('/api/add', (req, res) => {
-    //validate our ToDo
-    let item = req.body;
-    let check = ToDo.isValid(item);
-   
-    // If it is valid input, add to our list
-    if(check) {
-        let newItem = ToDo.packageItem(item);
-        aList.add_item(newItem);
-        console.log('added a new item');
-        res.send('added new item')
-    } else {
-        // If invalid input let the user know
-        res.status(400).send("Bad input");
-    }
+  // validate our ToDo
+  const item = req.body;
+  const check = ToDo.isValid(item);
+
+  // If it is valid input, add to our list
+  if (check) {
+    const newItem = ToDo.packageItem(item);
+    aList.addItem(newItem);
+    console.log('added a new item');
+    res.send('added new item');
+  } else {
+    // If invalid input let the user know
+    res.status(400).send('Bad input');
+  }
 });
 
 
 app.put('/api/list/:id', (req, res) => {
-    
-    // confirm the item exists in the list
-    const item = aList.get_list().find(c => c.id === req.params.id);
-    if(!item) res.status(404).send('Could not find an item with that name');
+  // confirm the item exists in the list
+  const item = aList.getList().find(c => c.id === req.params.id);
+  if (!item) res.status(404).send('Could not find an item with that name');
 
-    if(ToDo.isValid(req.body)){
-        aList.update_item(req.params.id, req.body);
-        res.send('Update completed');
-    } else {
-        console.log('Bad update');
-        res.status(400).send('Bad update');
-    }
-   
+  if (ToDo.isValid(req.body)) {
+    aList.updateItem(req.params.id, req.body);
+    res.send('Update completed');
+  } else {
+    console.log('Bad update');
+    res.status(400).send('Bad update');
+  }
 });
 
 
--- todo.js
+++ todo.js
@@ -1,51 +1,49 @@
 
 class ToDo {
-    constructor(name) {
-        this._listName = name;
-        this.list = [];
-    }
-    add_item(item) {
-        this.list.push(item);
-    }
-    get_list() {
-        return this.list;
-    }
+  constructor(name) {
+    this.listName = name;
+    this.list = [];
+  }
 
-    update_item(id, change) {
-        let index = this.list.findIndex((obj => obj.id == id));
-        
-        this.list[index] = change;
-        return;
+  addItem(item) {
+    this.list.push(item);
+  }
+
+  getList() {
+    return this.list;
+  }
+
+  updateItem(id, change) {
+    const index = this.list.findIndex((obj => obj.id === id));
+
+    this.list[index] = change;
+  }
+
+  static isValid(item) {
+    // console.log(item);
+    if (!item.id) {
+      return false;
+    } if (!item.name) {
+      return false;
+    } if (!item.description) {
+      return false;
+    } if (!item.tags) {
+      return false;
+    } if (!item.status) {
+      return false;
     }
+    return true;
+  }
 
-    static isValid(item) {
-        //console.log(item);
-        if(!item.id) {
-            return false;
-        } else if(!item.name) {
-            return false;
-        } else if (!item.description) {
-            return false;
-        } else if (!item.tags) {
-            return false;
-        } else if (!item.status) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    static packageItem(item){
-        const newItem = {
-            name:item.name,
-            description:item.description,
-            tags:item.tags,
-            status:item.status
-        }
-        return newItem;
-    }
-
-
+  static packageItem(item) {
+    const newItem = {
+      name: item.name,
+      description: item.description,
+      tags: item.tags,
+      status: item.status,
+    };
+    return newItem;
+  }
 }
 
 module.exports = ToDo;
